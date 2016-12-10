1a88fd13b7e9384ba9458eee054f374a5e1ad04b
Angelica Gonzalez & John Winslow & ArlenePerez <pair+agonzalez0515.john-winslow.techforchange@devbootcamp.com>
2016-12-10 13:51:07 -0800

    iterate through response object to pull out attributes for tweets

--- app/assets/javascripts/models.js
+++ app/assets/javascripts/models.js
@@ -1,37 +1,35 @@
 $(document).ready(function() {
-  fetchRecentTweets();
-
+  handleRecentTweets();
 })
 
-
-
-
 //model function
-function fetchRecentTweets() {
-     var requestPromise = $.ajax({
-       url:"/tweets/recent",
-       method:"GET",
-       })
+function fetchRecentTweets(){
+  var requestPromise = $.ajax({
+    url:"/tweets/recent",
+    method:"GET"
+  })
+  // console.log(requestPromise)
+  return requestPromise
+}
 
-     .done(function(response){
-        console.log(response)
-     })
-  }
-//
-// //Controller style function
-// // Notice that it uses our model function and our view function!
-// function handleRecentTweets() {
-//   var promiseFromAjax = fetchRecentTweets();
-//
-// //Here the controller function attaches something with .done()
-// // so that it can use the view when the data comes back.
-//   promiseFromAjax.done(function(response) {
-//   showRecentTweets(response)
-//   })
-// }
-//
-//
-// //View style function
-// function showRecentTweets(recentTweets) {
-//   //  $("#temperature").html("The temperature will be " + weatherInfo.temp)
-// }
+
+// Controller style function
+// Notice that it uses our model function and our view function!
+function handleRecentTweets() {
+  var promiseFromAjax = fetchRecentTweets();
+
+//Here the controller function attaches something with .done()
+// so that it can use the view when the data comes back.
+  promiseFromAjax.done(function(response) {
+    for(var i=0; i < response.length; i++){
+      console.log(response[i])
+    }
+
+  })
+}
+
+// //
+// // //View style function
+// // function showRecentTweets(recentTweets) {
+// //    $("#tweets-container").html("The temperature will be " + weatherInfo.temp)
+// // }
